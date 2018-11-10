62d1ea7ffadcf5743864f3c7e841a2ee2811f5a9
alexiabl <a1b9l9d6>
2018-11-10 15:26:10 +0100

    ininitial commit with navigation bar and some fragments in place to start development

---
+++ app/src/main/java/com/gifty/hci/gifty/MainActivity.java
@@ -0,0 +1,44 @@
+package com.gifty.hci.gifty;
+
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.design.widget.BottomNavigationView;
+import android.support.v4.app.Fragment;
+import android.support.v7.app.AppCompatActivity;
+import android.view.MenuItem;
+
+public class MainActivity extends AppCompatActivity {
+
+    private BottomNavigationView.OnNavigationItemSelectedListener navigationItemSelectedListener
+            = new BottomNavigationView.OnNavigationItemSelectedListener() {
+        @Override
+        public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
+            Fragment selectedFragment = null;
+            switch(menuItem.getItemId()){
+                case R.id.nav_home:
+                    selectedFragment = new HomeFragment();
+                    break;
+                case R.id.nav_search_friends:
+                    selectedFragment = new SearchFriendsFragment();
+                    break;
+                case R.id.nav_notifications:
+                    selectedFragment = new NotificationsFragment();
+                    break;
+                case R.id.nav_profile:
+                    selectedFragment = new ProfileFragment();
+                    break;
+            }
+            getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container,selectedFragment).commit();
+            return true;
+        }
+    };
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        BottomNavigationView navigation = (BottomNavigationView) findViewById(R.id.nav_bar);
+        navigation.setOnNavigationItemSelectedListener(navigationItemSelectedListener);
+    }
+}
