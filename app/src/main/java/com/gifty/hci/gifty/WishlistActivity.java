4b4867bcc9bf3935c8bcba4cc819d6235070f9de
alexiabl <a1b9l9d6>
2018-11-22 16:42:27 +0100

    Added my missing classes

---
+++ app/src/main/java/com/gifty/hci/gifty/WishlistActivity.java
@@ -0,0 +1,14 @@
+package com.gifty.hci.gifty;
+
+import android.os.Bundle;
+import android.app.Activity;
+
+public class WishlistActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_wishlist);
+    }
+
+}
